cdb5cc786c6d8fbd75e3ac62f7725e7cf7907f60
jpdunc23 <jpdunc23@users.noreply.github.com>
2019-05-14 14:36:05 -0700

    Full nClass packaging (#5)
    * Allow for full interface nClasses in package generation
    
    * Add 'quoted' argument to build_compiled_nClass() to return
      an unevaluated R6::R6Class() expression
    * Add 'nClass_interface' argument to writePackage(), which
      with value 'full' writes the R6:R6Class code to the package's
      R directory.
    * Add a test of full interface nClass packaging in test-packaging.R
    
    * Use the compiled genericInterfaceC class constructor as newCobjFun
    
    * In writePackage(), change 'nClass_interface' character vector argument to
      'nClass_full_interface' boolean argument to include the full interface

--- nCompiler/tests/testthat/test-packaging.R
+++ nCompiler/tests/testthat/test-packaging.R
@@ -41,10 +41,44 @@ test_that("writePackage and buildPackage work for nClass",
                 )
               )
             )
-            writePackage(nc1, package.name = "nc1Package", clean = TRUE)
+            writePackage(
+              nc1, package.name = "nc1Package", clean = TRUE,
+              nClass_full_interface = FALSE
+            )
             buildPackage("nc1Package", dir = ".", lib = "test_package_nc1Package_lib")
             expect_true(require("nc1Package", lib.loc = "test_package_nc1Package_lib"))
             expect_true(is.function(nc1Package::new_nc1))
             obj <- nc1Package::new_nc1()
             expect_equal(method(obj, 'cp1')(2), 3)
           })
+
+test_that("writePackage and buildPackage work for nClass with full interface",
+{
+  nCompiler:::nFunctionIDMaker(reset = TRUE)
+  nCompiler:::nClassIDMaker(reset = TRUE)
+
+  nc2 <- nClass(
+    classname = "nc2",
+    Rpublic = list(
+      p1 = function(x) x+1
+    ),
+    Cpublic = list(
+      x = 'numericScalar',
+      cp1 = nFunction(
+        fun = function(x = double()) {
+          ans <- x+1
+          return(ans)
+          returnType(double())
+        }
+      )
+    )
+  )
+  writePackage(
+    nc2, package.name = "nc2Package", clean = TRUE
+  )
+  expect_true(buildPackage("nc2Package", dir = ".", lib = "test_package_nc2Package_lib"))
+  expect_true(isCompiledNCgenerator(nc2Package::nc2))
+  obj <- nc2Package::nc2$new()
+  expect_true(inherits(obj, "nClass"))
+  expect_equal(obj$cp1(2), 3)
+})
