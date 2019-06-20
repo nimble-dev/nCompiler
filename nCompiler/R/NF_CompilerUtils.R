42702ecf7bab3adb889d099bd68bae620eadd6c9
jpdunc23 <jpdunc23@users.noreply.github.com>
2019-06-19 19:14:57 -0700

    Only mangle argument names that conflict with keys words in C++ language and cmath library (#18)
    * Fix recursive unlisting bugs in math_test_suite() and test-math.R
    
    * Add compilerUtilsEnv to hold cpp_keywords which should be mangled in C++ output

--- nCompiler/R/NF_CompilerUtils.R
+++ nCompiler/R/NF_CompilerUtils.R
@@ -1,3 +1,9 @@
+compilerUtilsEnv <- new.env()
+
+assignInCompilerUtilsEnv <- function(name, value) {
+  compilerUtilsEnv[[name]] <- value
+}
+
 make_cpp_filebase <- function(cpp_code_name) {
   paste0(Rname2CppName(cpp_code_name), '_c_')
 }
@@ -47,11 +53,47 @@ nf_substituteExceptFunctionsAndDollarSigns <- function(code, subList) {
     stop(paste("Error doing replacement for code ", deparse(code)))
 }
 
+assignInCompilerUtilsEnv(
+  'cpp_keywords',
+  c(
+    # C++ language keywords
+    'alignas', 'alignof', 'and', 'and_eq', 'asm', 'auto', 'bitand', 'bitor',
+    'bool', 'break', 'case', 'catch', 'char', 'char16_t', 'char32_t', 'class',
+    'compl', 'const', 'constexpr', 'const_cast', 'continue', 'decltype',
+    'default', 'delete', 'do', 'double', 'dynamic_cast', 'else', 'enum',
+    'explicit', 'export', 'extern', 'false', 'float', 'for', 'friend', 'goto',
+    'if', 'inline', 'int', 'long', 'mutable', 'namespace', 'new', 'noexcept',
+    'not', 'not_eq', 'nullptr', 'operator', 'or', 'or_eq', 'private',
+    'protected', 'public', 'register', 'reinterpret_cast', 'return', 'short',
+    'signed', 'sizeof', 'static', 'static_assert', 'static_cast', 'struct',
+    'switch', 'template', 'this', 'thread_local', 'throw', 'true', 'try',
+    'typedef', 'typeid', 'typename', 'union', 'unsigned', 'using', 'virtual',
+    'void', 'volatile', 'wchar_t', 'while', 'xor', 'xor_eq',
+    # cmath keywords
+    'cos', 'sin', 'tan', 'acos', 'asin', 'atan', 'atan2', 'cosh', 'sinh',
+    'tanh', 'acosh', 'asinh', 'atanh', 'exp', 'frexp', 'ldexp', 'log', 'log10',
+    'modf', 'exp2', 'expm1', 'ilogb', 'log1p', 'log2', 'logb', 'scalbn',
+    'scalbln', 'pow', 'sqrt', 'cbrt', 'hypot', 'erf', 'erfc', 'tgamma',
+    'lgamma', 'ceil', 'floor', 'fmod', 'trunc', 'round', 'lround', 'llround',
+    'rint', 'lrint', 'llrint', 'nearbyint', 'remainder', 'remquo', 'copysign',
+    'nan', 'nextafter', 'nexttoward', 'fdim', 'fmax', 'fmin', 'fabs', 'abs',
+    'fma', 'fpclassify', 'isfinite', 'isinf', 'isnan', 'isnormal', 'signbit',
+    'isgreater', 'isgreaterequal', 'isless', 'islessequal', 'islessgreater',
+    'isunordered', 'math_errhandling', 'INFINITY', 'NAN', 'HUGE_VAL',
+    'HUGE_VALF', 'HUGE_VALL', 'MATH_ERRNO', 'MATH_ERREXCEPT', 'FP_FAST_FMA',
+    'FP_FAST_FMAF', 'FP_FAST_FMAL', 'FP_INFINITE', 'FP_NAN', 'FP_NORMAL',
+    'FP_SUBNORMAL', 'FP_ZERO', 'FP_ILOGB0', 'FP_ILOGBNAN', 'double_t',
+    'float_t'
+  )
+)
+
 mangleArgumentNames <- function(argNames) {
-    ans <- character()
-    if(length(argNames) > 0)
-        ans <- paste0("ARG",
-                      seq_along(argNames),
-                      "_",
-                      Rname2CppName(argNames),"_")
+  ans <- character()
+  if(length(argNames) > 0) {
+    conflicts <- which(argNames %in% compilerUtilsEnv$cpp_keywords)
+    new_names <- paste0("ARG_", Rname2CppName(argNames[conflicts]), "_")
+    ans <- argNames
+    ans[conflicts] <- new_names
+  }
+  invisible(ans)
 }
