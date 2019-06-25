b09158e40797a56e9a513641b18f600f433bb53c
perrydv <pdevalpine@berkeley.edu>
2019-06-25 10:23:40 -0700

    fixed end index. added test.  added Rexecution.R.

--- nCompiler/tests/testthat/test-TBB.R
+++ nCompiler/tests/testthat/test-TBB.R
@@ -1,31 +1,31 @@
 context("Testing parallelization via TBB.")
 
-nc <- nClass(
-  Cpublic = list(
-    go = nFunction(
-      fun = function(x = 'numericVector') {
-        y <- x
-        parallel_for(i, 1:10,
-                     {
-                       y[i] <- 2 * x[i]
-                     },
-                     "x",
-                     "y")
-        return(y)
-      },
-      returnType = 'numericVector'
+test_that("Simple parallel example works", {
+  nc <- nClass(
+    Cpublic = list(
+      go = nFunction(
+        fun = function(x = 'numericVector') {
+          y <- x
+          parallel_for(i, 1:10,
+                       {
+                         y[i] <- 2 * x[i]
+                       },
+                       "x",
+                       "y")
+          return(y)
+        },
+        returnType = 'numericVector'
+      )
     )
   )
-)
 
-Cnc <- nCompile_nClass(nc, control = list(endStage = "makeCppDef"))
-class(Cnc)
-writeCode(Cnc$generate())
-writeCode(Cnc$generate(TRUE))
-
-set_nOption("showCompilerOutput", TRUE)
-Cnc <- nCompile_nClass(nc)
-nc1 <- nc$new()
-## nc1$go(101:110) ## does not run uncompiled
-Cnc1 <- Cnc$new()
-Cnc1$go(101:110) ## runs in parallel
+  # Cnc <- nCompile_nClass(nc, control = list(endStage = "makeCppDef"))
+  # writeCode(Cnc$generate())
+  # writeCode(Cnc$generate(TRUE))
+  
+  Cnc <- nCompile_nClass(nc)
+  nc1 <- nc$new()
+  Cnc1 <- Cnc$new()
+  expect_equal(nc1$go(101:110), 2*(101:110))
+  expect_equal(Cnc1$go(101:110), array(2*(101:110)))
+})
