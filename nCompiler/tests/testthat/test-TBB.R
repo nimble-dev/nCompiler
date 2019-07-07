d89f41cf370823bc5e8b008c8bba95af96288d7d
perrydv <pdevalpine@berkeley.edu>
2019-07-07 20:37:28 +0200

    Export parallel_for.  Fix test-TBB

--- nCompiler/tests/testthat/test-TBB.R
+++ nCompiler/tests/testthat/test-TBB.R
@@ -7,26 +7,9 @@ test_that("Simple parallel example works", {
         fun = function(x = 'numericVector') {
           y <- x
           parallel_for(i, 1:10,
-                       {
-                         y[i] <- 2 * x[i]
-                       },
-                       "x",
-                       "y")
-          return(y)
-        },
-        returnType = 'numericVector'
-      )
-    )
-  )
-
-  nc <- nClass(
-    Cpublic = list(
-      go = nFunction(
-        fun = function(x = 'numericVector') {
-          y <- x
-          for(i in 1:10) {
-            y[i] <- 2 * x[i]
-          }
+                       {y[i] <- 2 * x[i]},
+                       "x", ## copy for each thread
+                       "y") ## share across threads
           return(y)
         },
         returnType = 'numericVector'
@@ -37,9 +20,9 @@ test_that("Simple parallel example works", {
   # Cnc <- nCompile_nClass(nc, control = list(endStage = "makeCppDef"))
   # writeCode(Cnc$generate())
   # writeCode(Cnc$generate(TRUE))
-  Cnc <- nCompile_nClass(nc)
+  Cnc <- nCompile(nc)
   nc1 <- nc$new()
   Cnc1 <- Cnc$new()
   expect_equal(nc1$go(101:110), 2*(101:110))
   expect_equal(Cnc1$go(101:110), array(2*(101:110)))
-})
+1})
