427196d8913795e654458dceb6221007b4a856f4
Seligman <seligman@localhost.localdomain>
2021-05-06 16:37:07 -0700

    Workflow check.

--- nCompiler/inst/include/nCompiler/nCompiler_serialization_mgr.h
+++ nCompiler/inst/include/nCompiler/nCompiler_serialization_mgr.h
@@ -2,28 +2,46 @@
 #define _NCOMP_SERIALIZATION_MGR
 
 #include "nCompiler_class_interface.h"
-// Rough draft of C++ serialization manager class
-// I want to make this as similar as possible to the design of an nClass
-// for simplicity and code re-use.
-class serialization_mgr : public genericInterfaceC<serialization_mgr> {
+
+
+/**
+   @brief C++ serialization prototype, mimicking design of 'nClass' for simplicity and re-use.
+ */
+class serialization_mgr : public genericInterfaceC<serialization_mgr> { ///< CRTP
 public:
   typedef std::unique_ptr<genericInterfaceBaseC> unique_base_ptr;
-  std::vector< unique_base_ptr > objects;
+  std::vector< unique_base_ptr > objects; ///< Core-side cached objects.
+
+  /**
+     @brief Caches an object for serialization.
+
+     @param Sextptr is a raw expression pointer to the object being cached.
+
+     @return index into vector of the serialized objects.
+   */
   int add_extptr(SEXP Sextptr) {
-    std::cout<<"hello world\n";
+    std::cout<< "Greetings from add_extptr" << endl;
     return 0;
-    genericInterfaceBaseC *baseobj = reinterpret_cast<genericInterfaceBaseC*>(reinterpret_cast<shared_ptr_holder_base*>(R_ExternalPtrAddr(Sextptr))->get_ptr());
-    unique_base_ptr unique_baseobj(baseobj);
-    objects.push_back( std::move(unique_baseobj) );
+
+    objects.emplace_back(reinterpret_cast<genericInterfaceBaseC*>(reinterpret_cast<shared_ptr_holder_base*>(R_ExternalPtrAddr(Sextptr))->get_ptr()));
+
     return objects.size() - 1;
   }
+
+  /**
+     @brief Looks up serialized object by index.
+
+     @return R-style expression pointer to bserialized object.
+   */
   SEXP get_extptr(int i) {
     SEXP Sans = PROTECT((objects[i].release())->make_deserialized_return_SEXP());
     UNPROTECT(1);
     return(Sans);
   }
+
   template<class Archive>
     void _SERIALIZE_ ( Archive & archive );
+  
   SEXP make_deserialized_return_SEXP ( );
 };
 
