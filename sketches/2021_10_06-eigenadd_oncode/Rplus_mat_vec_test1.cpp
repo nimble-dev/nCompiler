a85e0d96572ac285febf9b25ee22c2c5b158a413
jmhewitt <joshua.hewitt@duke.edu>
2021-10-07 11:49:22 -0600

    demonstrating compilation issue with chaining

--- sketches/2021_10_06-eigenadd_oncode/Rplus_mat_vec_test1.cpp
+++ sketches/2021_10_06-eigenadd_oncode/Rplus_mat_vec_test1.cpp
@@ -4,7 +4,20 @@
 // [[Rcpp::export]]
 Eigen::Tensor<double, 2> Rplus_mat_vec_test1(Eigen::Tensor<double, 2> a,
 					     Eigen::Tensor<double, 1> b)  {
-  Eigen::Tensor<double, 2> ans = Rplus_2_1_(a, b); // alt: Rplus<2, 1>(a, b)
+  
+  // chaining with an explicit operator works!
+  auto ans = Rplus_2_1_(a, b) + Rplus_2_1_(a, b);
+  
+  // // chaining with an explicit operator works!
+  // auto ans = Rplus_2_1_(a, b) + a;
+  
+  // // compilation error when attempting to chain without an explicit operator.
+  // // chaining without an explicit operator might be important if we want to 
+  // // return unevaluated Eigen op's that use a combination of operators, some 
+  // // of which may be user-defined.
+  // auto step1 = Rplus_2_1_(a,b);
+  // auto step2 = Rplus_2_1_(step1,a);
+  
   // We want Rplus_2_1_ to return an Eigen op (delayed evaluation object)
   // in case we have ans = Rplus_2_1_(a, b) + c.
   return ans;
