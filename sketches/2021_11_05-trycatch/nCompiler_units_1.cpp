4910e57c14b52c390d10ad8e3aadeee6c1a79959
jmhewitt <joshua.hewitt@duke.edu>
2021-11-05 16:29:23 -0600

    add commentary

--- sketches/2021_11_05-trycatch/nCompiler_units_1.cpp
+++ sketches/2021_11_05-trycatch/nCompiler_units_1.cpp
@@ -55,6 +55,10 @@ using namespace Rcpp;
 // [[Rcpp::depends(nCompiler)]]
 // [[Rcpp::depends(Rcereal)]]
 
+// build a crude stack trace of nFunctions.  each level of the trace captures
+// the function name in [StringPair].first and the step being executed in
+// [StringPair].second.  The step may be updated several times within a single
+// function call, as the function executes.
 typedef std::pair<std::string, std::string> StringPair;
 static std::vector<StringPair> msgs;
 #define NC_TRY_PUSHFUN(x) { msgs.emplace_back(StringPair(x, "")); }
@@ -65,12 +69,25 @@ static std::vector<StringPair> msgs;
 Eigen::Tensor<double, 2>  nFun_1_NFID_1 ( Eigen::Tensor<double, 2> x, Eigen::Tensor<double, 2> y )  {
 RESET_EIGEN_ERRORS
 try{
+    // TODO: insert NC_TRY_PUSHFUN into nFunction code during nCompiler debugging stage
     NC_TRY_PUSHFUN("nf");  // nf is the nFunction object's name in R
     Eigen::Tensor<double, 2> z;
+    // TODO: insert NC_TRY_SETSTEP into nFunction code during nCompiler debugging stage,
+    //   but do not do this recursively; only insert NC_TRY_SETSTEP calls on
+    //   top-level code$arg[[i]] entries
     NC_TRY_SETSTEP("z = x+y");
     z = x+y;
+    // TODO: insert NC_TRY_POPFUN into nFunction code during nCompiler debugging stage;
+    //   the code tree will need to be recursively searched to hunt for return
+    //   statements nested within code since return statements can be used to
+    //   end processing "early".
     NC_TRY_POPFUN;
     return(z);
+    // TODO: insert NC_TRY_POPFUN as the last code$arg element in nFunction
+    //  code during nCompiler debugging stage; this is only necessary for an
+    //  nFunction that does not end with a return statement, as may occur if
+    //  an nFunction is used with reference semantics to modify an external
+    //  object
     NC_TRY_POPFUN;
 } catch(const std::exception & e) {
     std::string s;
