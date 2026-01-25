7a3b332766751b6a792383c8e64f5952cb40cb7f
Christopher Paciorek <paciorek@stat.berkeley.edu>
2026-01-25 12:53:59 -0800

    Add a bit more testing of Rbrowser().

--- nCompiler/tests/testthat/nCompile_tests/test-Rbrowser.R
+++ nCompiler/tests/testthat/nCompile_tests/test-Rbrowser.R
@@ -34,11 +34,64 @@ testthat("Simple Rbrowser call works", {
   cfoo <- nCompile(foo)
   cfoo()
 
+  # Check that we get into a browser
+  # with view list containing input arguments
+  foo <- nFunction(
+    fun = function(y) {
+      x <- 1:5
+      Rbrowser(view = c("x", "y"))
+    },
+    argTypes = list(y = 'numericScalar')
+  )
+  foo()
+  cfoo <- nCompile(foo)
+  cfoo()
+  
+  # Check that we get into a browser
+  # with view list containing vars of various types
+  foo <- nFunction(
+    fun = function(z = integerVector) {
+      # w <- "hello"
+      x <- nMatrix(type = 'integer', nrow = 2, ncol = 12, value = z)
+      # y <- nArray(type = 'integer', dim = c(2,3,4), value = z)
+      Rbrowser(view = c("x"))
+    }
+  )
+  foo()
+  cfoo <- nCompile(foo)
+  cfoo(1:24)
+
+  # check that can access member data in nClass object (awkwardly at the moment)
+  nc1 <- nClass(
+      classname = "nc1",
+      Rpublic = list(
+          Rv = 1,
+          Rfoo = function(x) x+1
+      ),
+      Cpublic = list(
+          Cv = 'numericScalar',
+          Cfoo = nFunction(
+              fun = function(x) {
+                  Cv <- Cv + 100
+                  Rbrowser(view = 'x') 
+                  return(x+1)
+              },
+              argTypes = list(x = 'numericScalar'),
+              returnType = 'numericScalar')
+      )
+  )
+  cnc1 <- nCompile(nc1)
+  obj <- cnc1$new()
+  obj$Cv <- 7
+  obj$Cfoo(5)
+  ## Use `get('obj')$Cv` to see member data.
+  ## Use `local_obj <- get('obj'); local_obj$Cv <- 1000` to modify.
+
+  
   # to do:
   # - add a "modify" list of inputs whose modified
   # values will be copied back into C++
-  # - check that various types work,
-  #  includes variables that were arguments
+  # - check that various types work (in progress above)
   # - work to make "self" work (will take some care)
 
 })
