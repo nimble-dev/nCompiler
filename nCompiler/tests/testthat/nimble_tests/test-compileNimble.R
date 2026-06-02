514aab8cc26ace914bfdfcb1480ea4bfed21bfdf
Christopher Paciorek <paciorek@stat.berkeley.edu>
2026-06-02 07:50:25 -0700

    Add a couple more compileNimble tests.

--- nCompiler/tests/testthat/nimble_tests/test-compileNimble.R
+++ nCompiler/tests/testthat/nimble_tests/test-compileNimble.R
@@ -26,7 +26,39 @@ test_that("compileNimble bridge works for one nimbleFunction object", {
   nf1 <- nf()
   Cnf1 <- nCompiler:::compileNimble(nf1)
   expect_identical(Cnf1$x, 1:2)
+
+  nf <- nimbleFunction(
+      setup = function() {x <- 2:3},
+      run = function(mult = double(0)) {
+          return(myfun(mult)); returnType(double())
+      },
+      methods = list(
+          myfun = function(mult = double(0)) {
+              return(x[1]*mult); returnType(double())
+          }
+      )
+  )
+  nf1 <- nf()
+  Cnf1 <- nCompiler:::compileNimble(nf1)
+  expect_identical(Cnf1$run(3), 6)
 })
+  
+test_that("compileNimble bridge works for two nimbleFunction objects", {
+  nf1 <- nimbleFunction(
+      run = function(x = double(0)) {
+          return(nf2(x))
+          returnType(double(0))
+      })
+  nf2 <- nimbleFunction(
+      run = function(x = double(0)) {
+          return(3*x)
+          returnType(double(0))
+      })
+  Cnfs <- nCompiler:::compileNimble(nf1,nf2)
+  expect_identical(Cnfs[[1]](5), 15)
+ 
+})
+
 ## NEXT STEPS:
 ## get a custom handler working
 ## try a test file from nimble using nClass
