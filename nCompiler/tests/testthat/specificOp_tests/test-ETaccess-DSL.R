c8de4605ad0487ce170c1fe6ddb74e8bca5a4ce3
perrydv <perrydv@users.noreply.github.com>
2026-06-04 12:34:25 -0700

    clean up last test in test-ETaccess-DSL, missed before

--- nCompiler/tests/testthat/specificOp_tests/test-ETaccess-DSL.R
+++ nCompiler/tests/testthat/specificOp_tests/test-ETaccess-DSL.R
@@ -75,26 +75,29 @@ test_that("as(obj[[var_name]], type) works", {
       x = 'numericVector'
     )
   )
-  nf <- nFunction(
-    function(obj = 'nc', var_name = 'string') {
-      v <- as(obj[[var_name]], 'numericVector')
-        return(v)
-      returnType('numericVector')
-    }
+  nc2 <- nClass(
+    Cpublic = list(
+      nf = nFunction(
+        function(obj = 'nc', var_name = 'string') {
+          v <- as(obj[[var_name]], 'numericVector')
+          return(v)
+          returnType('numericVector')
+        }
+      )
+    )
   )
   for(mode in c("R","non-pkg", "pkg")) {
     if(mode == "R") {
       obj <- nc$new()
-      foo <- nf
+      obj2 <- nc2$new()
     } else {
       package <- mode=="pkg"
-      comp <- nCompile(nc, nf, package = package)
+      comp <- nCompile(nc, nc2, package = package)
       obj <- comp$nc$new()
-      foo <- comp$nf
+      obj2 <- comp$nc2$new()
     }
     obj$x <- c(1.2, 2.3)
-    foo(obj, "x")
-    expect_equal(foo(obj, "x"), obj$x)
+    expect_equal(obj2$nf(obj, "x"), obj$x)
     rm(obj); gc()
   }
 })
